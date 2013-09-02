531c6eced2d7e726249de831e4abfef0fe2bb996
root <root@ns3.naicoms.com>
2013-09-02 17:09:26 +0300

    demo1

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
         </div>
 
         <div class="col-lg-6">
-          <h4>demo</h4>
+          <h4>Subheading</h4>
           <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
 
           <h4>Subheading</h4>
